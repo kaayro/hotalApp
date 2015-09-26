ad3f87a325e36f3af21d7f35d804f13509b3f996
Carlos Celma <solrac@igitsoft.com>
2015-09-26 12:17:35 -0500

    a

--- js/actions.js
+++ js/actions.js
@@ -2,10 +2,28 @@ var fn = {
 	init: function(){
 		if(!fn.islogged())
 			window.location.href = "#reg";
+		
+		//Funcionalidades de Registro
+		$('#regSend').click(fn.getRegister);
+		$('#takePhoto').click(mediaCapture.takePhoto);
+	},
+	ready:function(){
+		document.addEventListener("deviceready", fn.init, false);
 	},
 	islogged: function(){
 		return false;
+	},
+	getRegister: function(){
+		var nom = $('#regName').val();
+		var tel = $('#regTel').val();
+		var mail = $('#regMail').val();
+		if(nom != '' && tel != '' && mail != ''){
+			alert();
+			//Enviar datos al servidor
+		}else{
+			navigator.notification.alert('Todos los campos son requeridos', null, "Error de Registro", "Aceptar");
+		}
 	}
 };
 
-window.addEventListener("load", fn.init, false);
+$(fn.ready);
